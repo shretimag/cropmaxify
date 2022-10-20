14d79733f718459e5f8d9743d83482e017a9c792
Raghvan Pareek <63724417+I-karmayogi@users.noreply.github.com>
2022-10-21 02:37:17 +0530

    Add files via upload
    Commited inital files of the Application

---
+++ app.py
@@ -0,0 +1,71 @@
+import streamlit as st 
+# import streamlit_tags as tags
+import pandas as pd
+import numpy as np
+import os
+import pickle
+import warnings
+
+
+st.beta_set_page_config(page_title="Crop Recommender", page_icon="🌿", layout='centered', initial_sidebar_state="collapsed")
+
+def load_model(modelfile):
+	loaded_model = pickle.load(open(modelfile, 'rb'))
+	return loaded_model
+
+def main():
+    # title
+    html_temp = """
+    <div>
+    <h1 style="color:MEDIUMSEAGREEN;text-align:left;">CropMaxify Recommendation</h1>
+    </div>
+    """
+    st.markdown(html_temp, unsafe_allow_html=True)
+
+    col1,col2  = st.beta_columns([2,2])
+    
+    with col1: 
+        with st.beta_expander(" ℹ️ Information", expanded=True):
+            st.write("""
+            Crop recommendation is one of the most important aspects of precision agriculture. Crop recommendations are based on a number of factors. Precision agriculture seeks to define these criteria on a site-by-site basis in order to address crop selection issues. While the "site-specific" methodology has improved performance, there is still a need to monitor the systems' outcomes.Precision agriculture systems aren't all created equal. 
+            However, in agriculture, it is critical that the recommendations made are correct and precise, as errors can result in significant material and capital loss.
+            """)
+
+
+
+    with col2:
+        st.subheader("Lets find out what crop would be besst for you to grow in your farm!")
+        N = st.number_input("Nitrogen", 1,10000)
+        P = st.number_input("Phosporus", 1,10000)
+        K = st.number_input("Potassium", 1,10000)
+        temp = st.number_input("Temperature",0.0,100000.0)
+        humidity = st.number_input("Humidity in %", 0.0,100000.0)
+        ph = st.number_input("Ph", 0.0,100000.0)
+        rainfall = st.number_input("Rainfall in mm",0.0,100000.0)
+
+        feature_list = [N, P, K, temp, humidity, ph, rainfall]
+        single_pred = np.array(feature_list).reshape(1,-1)
+        
+        if st.button('Predict'):
+
+            loaded_model = load_model('ml.pkl')
+            prediction = loaded_model.predict(single_pred)
+            col1.write('''
+		    ## Results 
+		    ''')
+            col1.success(f"{prediction.item().title()} are recommended by us for your farm")
+    hide_menu_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    </style>
+    """
+
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+if __name__ == '__main__':
+	main()
